e215de852c063fc5f3acca1ea042940332502f40
Yousef Mahrous <yousefzentral@gmail.com>
2026-01-17 17:16:30 +0200

    init

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 // create an extended client
